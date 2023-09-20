bd3708b7431344e2f09c7b664d129f41fd6ab45c
sathishchinniah04 <87637647+sathishchinniah04@users.noreply.github.com>
2023-09-20 12:12:04 +0530

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  OmentalFrameworkExample
+//
+
+import Foundation
---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import { NativeModules, Platform } from 'react-native';
+
+const LINKING_ERROR =
+  `The package 'react-native-omental-framework' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+const OmentalFramework = NativeModules.OmentalFramework
+  ? NativeModules.OmentalFramework
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+export function multiply(a: number, b: number): Promise<number> {
+  return OmentalFramework.multiply(a, b);
+}
